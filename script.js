f60b139979884984f81b4a3bcae8bc99eda86e97
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-23 04:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const output = document.querySelector("#output");
+let arr = [1,2,3,4];
+
+function promise1(arr){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			arr = arr.filter((ele)=>ele%2===0);
+			resolve(arr);
+			
+		},1000)
+	})
+	
+}
+function promise2(arr){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			arr = arr.map((ele)=>ele*2);
+			resolve(arr);
+			
+		},2000)
+	})
+}
+promise1(arr).then((arr)=>{
+	output.textContent = arr.join(',');
+	return promise2(arr);
+}).then((arr)=>{
+	output.textContent = arr.join(',');
+	
+})
